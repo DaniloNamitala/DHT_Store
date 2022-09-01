fa49c96abb79ee07f5aa29bd75afa6bd19ba579d
Tulio <tuliocruz_@hotmail.com>
2022-09-01 15:37:37 -0300

    Tabela Cliente

--- dht_back/index.js
+++ dht_back/index.js
@@ -30,6 +30,19 @@ database.query("CREATE TABLE IF NOT EXISTS produto(\
   }
 );
 
+database.query("CREATE TABLE IF NOT EXISTS cliente(\
+  id INT AUTO_INCREMENT NOT NULL,\
+  nome VARCHAR(100) NOT NULL,\
+  cpf VARCHAR(20) NOT NULL,\
+  email VARCHAR(100) NOT NULL,\
+  dataNascimento datetime NOT NULL, \
+  senha VARCHAR(150) NOT NULL,\
+  CONSTRAINT cliente_pk PRIMARY KEY(id))",
+  (err, result) => {
+    if(err) console.log(err);
+  }
+);
+
 app.listen(3001, () => {
   console.log("Server Started");
 });
@@ -45,4 +58,19 @@ app.post("/insertProduct", (req, res) => {
       res.send(JSON.stringify({result: "SUCCESS"}))
     }
   });
+});
+
+app.post("/insertClient", (req, res) => {
+  let data = req.body
+  let SQL = "INSERT INTO cliente (nome, cpf, email, dataNascimento, senha) VALUES (?, ?, ?, ?, ?)"
+  
+  database.query(SQL, [data['name'], data['cpf'], data['email'], data['birthDate'], data['password']], (err, result) => {
+    if (err) { 
+      console.log(err)
+    } else {
+      res.send(JSON.stringify({result: "SUCCESS"}))
+    }
+  });
+
+  
 });
