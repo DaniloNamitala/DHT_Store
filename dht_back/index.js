e0be2584827427ffa6f09129a424506e2af2a1f7
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-09-06 01:00:42 -0300

    Rotas para deletar e editar produto

--- dht_back/index.js
+++ dht_back/index.js
@@ -1,22 +1,25 @@
-const express = require("express");
-const mysql = require("mysql");
+const express = require("express")
+const mysql = require("mysql")
 const cors = require("cors")
 
-const app = express();
+const app = express()
+
+// WSL iniciar mysql: sudo /etc/init.d/mysql start
+// mysql -u root -p
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  next();
-});
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  next()
+})
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 const database = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "password",
   database: 'dht_store'
-});
+})
 
 database.query("CREATE TABLE IF NOT EXISTS produto(\
   id INT AUTO_INCREMENT NOT NULL,\
@@ -26,9 +29,9 @@ database.query("CREATE TABLE IF NOT EXISTS produto(\
   descricao TEXT,\
   CONSTRAINT produto_pk PRIMARY KEY(id))",
   (err, result) => {
-    if(err) console.log(err);
+    if(err) console.log(err)
   }
-);
+)
 
 database.query("CREATE TABLE IF NOT EXISTS cliente(\
   id INT AUTO_INCREMENT NOT NULL,\
@@ -39,14 +42,53 @@ database.query("CREATE TABLE IF NOT EXISTS cliente(\
   senha VARCHAR(150) NOT NULL,\
   CONSTRAINT cliente_pk PRIMARY KEY(id))",
   (err, result) => {
-    if(err) console.log(err);
+    if(err) console.log(err)
   }
-);
+)
 
 app.listen(3001, () => {
-  console.log("Server Started");
-  });
-  
+  console.log("Server Started")
+})
+
+app.get("/getProductsList", (req, res) => {
+  let SQL = "SELECT * FROM produto"
+
+  database.query(SQL, (err, result) => {
+    if (!err) { 
+      res.send(result) 
+    } else {
+      res.send(JSON.stringify({result: "FAILED"}))
+    }
+  })
+})
+
+app.post("/deleteProduct", (req, res) => {
+  let productId = req.body['id']
+  let SQL = "DELETE FROM produto WHERE id=?"
+  database.query(SQL, [productId], (err, result) => {
+    if (err) { 
+      console.log(err)
+      res.send(JSON.stringify({result: "FAILED"}))
+    } else {
+      res.send(JSON.stringify({result: "SUCCESS"}))
+    }
+  })
+})
+
+app.post("/editProduct", (req, res) => {
+  let data = req.body
+  let SQL = "UPDATE produto SET nome=?, quantidade=?, preco=?, descricao=? WHERE id=?"
+
+  database.query(SQL, [data['title'], data['qty'], data['price'], data['description'], data["id"]],(err, result) => {
+    if (err) { 
+      console.log(err)
+      res.send(JSON.stringify({result: "FAILED"}))
+    } else {
+      res.send(JSON.stringify({result: "SUCCESS"}))
+    }
+  })
+})
+
 app.post("/insertProduct", (req, res) => {
   let data = req.body
   let SQL = "INSERT INTO produto (nome, quantidade, preco, descricao) VALUES (?, ?, ?, ?)"
@@ -54,11 +96,12 @@ app.post("/insertProduct", (req, res) => {
   database.query(SQL, [data['title'], data['qty'], data['price'], data['description']], (err, result) => {
     if (err) { 
       console.log(err)
+      res.send(JSON.stringify({result: "FAILED"}))
     } else {
       res.send(JSON.stringify({result: "SUCCESS"}))
     }
-  });
-});
+  })
+})
 
 app.post("/insertClient", (req, res) => {
   let data = req.body
@@ -67,26 +110,21 @@ app.post("/insertClient", (req, res) => {
   database.query(SQL, [data['name'], data['cpf'], data['email'], data['birthDate'], data['password']], (err, result) => {
     if (err) { 
       console.log(err)
+      res.send(JSON.stringify({result: "FAILED"}))
     } else {
       res.send(JSON.stringify({result: "SUCCESS"}))
     }
-  });
-
-  
-});
-
-app.get("/getProductsList", (req, res) => {
-  let SQL = "SELECT * FROM produto";
-
-  database.query(SQL, (err, result) => {
-    if (!err) res.send(result)
   })
 })
 
 app.get("/getClientList", (req, res) => {
-  let SQL = "SELECT * FROM cliente";
+  let SQL = "SELECT * FROM cliente"
 
   database.query(SQL, (err, result) => {
-    if (!err) res.send(result)
+    if (!err) { 
+      res.send(result) 
+    } else {
+      res.send(JSON.stringify({result: "FAILED"}))
+    }
   })
 })
