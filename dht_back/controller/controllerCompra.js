fc3f10523062a114a3b72731b51a1f82a818dd13
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-09-14 00:38:23 -0300

    Rota adicionar compra

--- dht_back/controller/controllerCompra.js
+++ dht_back/controller/controllerCompra.js
@@ -1,8 +1,16 @@
-//const compraDAO = require("../model/clienteDAO")
+const compraDAO = require("../model/compraDAO")
+const comprasDAO = require("../model/compraDAO")
 
 module.exports = {
   cadastrarCompra: (req, res) => {
     let data = req.body
+    comprasDAO.inserirCompra(data, (err, result)=> {
+      if (!err) {
+        res.send(JSON.stringify({result: "SUCCESS"}))
+      } else {
+        res.send(JSON.stringify({result: "FAILED"}))
+      }
+    })
   },
 
   listarCompras: (req, res) => {
